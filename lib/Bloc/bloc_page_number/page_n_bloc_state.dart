a2bea1f05c7b42123d214c91d21ca6c795414e85
Hossein Mohammadi <132124730+HosseinMohammadiii@users.noreply.github.com>
2024-12-10 19:05:04 +0330

    Update page_n_bloc_state.dart

--- lib/Bloc/bloc_page_number/page_n_bloc_state.dart
+++ lib/Bloc/bloc_page_number/page_n_bloc_state.dart
@@ -1,11 +1,14 @@
+// Class to manage navigation states in the application
 class NavigationState {
+  // Variable to the current page being viewed
   ViewPage viewPage;
-
+  // Constructor to initialize the current page
   NavigationState(
     this.viewPage,
   );
 }
 
+// Enum to define the different pages within the application
 enum ViewPage {
   backScreen,
   category,
@@ -17,11 +20,15 @@ enum ViewPage {
   registerRentHomeAdvertising,
 }
 
+// Class to manage different status modes
 final class StatusModeState {
+  // Variable to the current status mode
   final StatusMode statusMode;
+  // Constructor to initialize the status mode
   StatusModeState(this.statusMode);
 }
 
+// Enum to define different statuses for a property
 enum StatusMode {
   apartment,
   home,
