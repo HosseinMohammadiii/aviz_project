a6ff10657ffcde79095db58d804c527e3a052f46
Hossein Mohammadi <132124730+HosseinMohammadiii@users.noreply.github.com>
2024-12-10 19:34:49 +0330

    Update dio_provider.dart

--- lib/DataFuture/NetworkUtil/dio_provider.dart
+++ lib/DataFuture/NetworkUtil/dio_provider.dart
@@ -3,6 +3,7 @@ import 'package:aviz_project/DataFuture/NetworkUtil/base_url.dart';
 import 'package:dio/dio.dart';
 
 class DioProvider {
+  // Creates and configures a `Dio` instance with headers.
   static Dio crateDio() {
     Dio dio = Dio(
       BaseOptions(
@@ -15,7 +16,8 @@ class DioProvider {
     );
     return dio;
   }
-
+   // Creates and configures a `Dio` instance without headers.
+  // This can be used for requests that do not require authentication.
   static Dio createDioWithoutHeader() {
     Dio dio = Dio(BaseOptions(
       baseUrl: BaseUrl.baseUrl,
