61b456344411454053a3b01c78c961a752f7bf65
Hossein Mohammadi <132124730+HosseinMohammadiii@users.noreply.github.com>
2024-12-11 23:40:43 +0330

    Update constraintboxsize.dart

--- lib/extension/constraintboxsize.dart
+++ lib/extension/constraintboxsize.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+// This extension adds a method that restricts the maximum width of a widget to 1200.
 extension Constraintboxsize on BoxConstraints {
+  // Restricts the maximum width of the constraints to 1200.
   BoxConstraints constrainstSize() {
     return const BoxConstraints(
       maxWidth: 1200,
