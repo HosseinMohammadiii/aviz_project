8e881b558274e52ab06d8a0a0fe25fcb545bfccb
Hossein Mohammadi <132124730+HosseinMohammadiii@users.noreply.github.com>
2024-12-11 23:35:29 +0330

    Update screens.dart

--- lib/class/screens.dart
+++ lib/class/screens.dart
@@ -3,6 +3,7 @@ import 'package:aviz_project/screen/manegement_add_advertising_screen.dart';
 import 'package:aviz_project/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
+//Screen's list for use in bottom navigation bar.
 List<Widget> screen = const [
   AccountScreen(),
   ManagementAddAdvertisingScreen(),
