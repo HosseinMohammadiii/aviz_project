a21062d6c8e16259ffc844876b88ecc21be86df3
Hossein Mohammadi <132124730+HosseinMohammadiii@users.noreply.github.com>
2024-12-11 23:48:46 +0330

    Update price_extension.dart

--- lib/extension/price_extension.dart
+++ lib/extension/price_extension.dart
@@ -1,5 +1,6 @@
 import 'package:intl/intl.dart';
 
+// This extension adds a `formatter` method to `int` that formats the number as a currency string using the Persian locale ('fa-IR').
 extension PriceExtension on int {
   String formatter() {
     NumberFormat currencyFormat =
